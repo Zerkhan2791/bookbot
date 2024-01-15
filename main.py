419310de1e0d72bc9d4cb7979d66770a74995165
Zerkhan2791 <warpspeed177@gmail.com>
2024-01-15 11:25:09 +0000

    optimised from community feedback

--- main.py
+++ main.py
@@ -3,23 +3,21 @@ import string
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = get_num_words(text)
+    num_words = len(text.split())
     analysis_unparsed = char_analysis(text)
-    list_letters = list(analysis_unparsed.keys())
-    num_letters = list(analysis_unparsed.values())
+    # list_letters = list(analysis_unparsed.keys())
+    # num_letters = list(analysis_unparsed.values())
     
     print(f"--- Beginning report of {book_path} ---\n")
     print(f"{num_words} words found in the document.\n")
     
-    for i in range(len(num_letters)):
-        print(f"The '{list_letters[i]}' character was found {num_letters[i]} times.")
+    for letter, nb in analysis_unparsed.items():
+        print(f"The '{letter}' character was found {nb} times.")
+
+    # for i in range(len(num_letters)):
+    #     print(f"The '{list_letters[i]}' character was found {num_letters[i]} times.")
 
     print(f"\n--- Ending report of {book_path} ---")
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
-
 
 def get_book_text(path):
     with open(path, encoding='UTF-8') as f:
